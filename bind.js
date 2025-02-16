bc8e4e9e8a90e4e535bbb9fe3b3842f032697377
jeevan-aj <jeevan@finanshels.com>
2025-02-17 03:12:44 +0530

    learned bind

--- bind.js
+++ bind.js
@@ -4,12 +4,28 @@ let person1Details = {
     name:"jee",
     age:'23',
     job:'unemployeed',
-    sayHi :function sayHi (par1,par2){
-        console.log("Hi" +" "+""+ this.name,par1,par2)
+    sayHi :function sayHi (par1,par2,par3){
+        console.log("Hi" +" "+""+ this.name,par1,par2,par3)
     }
 }
 
-const sayHifunc = person1Details.sayHi
+const sayHiFunc = person1Details.sayHi
 
-const returnedSayHiFunc =sayHifunc.bind(person1Details,"This is Bind Arg 1","This is bind arg 2")
+const returnedSayHiFunc =sayHiFunc.bind(person1Details,"This is Bind Arg 1","This is bind arg 2")
 returnedSayHiFunc()
+
+
+//polyfill for bind
+function myBind(thisArg,...parameters) {
+    let callingFunction = this
+
+    return function(){
+        callingFunction.apply(thisArg,parameters)
+    }
+}
+
+Function.prototype.myBind = myBind
+
+
+const customFunc = sayHiFunc.myBind(person1Details,"This is Bind Arg 1","This is bind arg 2","1000")
+customFunc()
